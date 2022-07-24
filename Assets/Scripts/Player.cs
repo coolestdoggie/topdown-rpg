c7a45417f8bb9777b1eb0b82682ec4c5a4e2e47d
coolestdoggie <coolestdoggie1@gmail.com>
2022-07-24 14:49:06 +0200

    add manual collision behavior

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,10 +25,20 @@ public class Player : MonoBehaviour
         
 
         hit = Physics2D.BoxCast(transform.position,boxCollider.size,
-            0,
-            new Vector2(0, moveDelta.y),
-            Mathf.Abs(moveDelta.y * Time.deltaTime));
-
-        transform.Translate(moveDelta * Time.deltaTime);
+                                0,
+                                new Vector2(0, moveDelta.y),
+                                Mathf.Abs(moveDelta.y * Time.deltaTime),
+                                LayerMask.GetMask("Actor","Blocking"));
+        
+        if (hit.collider == null) transform.Translate(0f, moveDelta.y * Time.deltaTime, 0f);
+        
+        hit = Physics2D.BoxCast(transform.position,boxCollider.size,
+                                0,
+                                new Vector2(moveDelta.x,0),
+                                Mathf.Abs(moveDelta.x * Time.deltaTime),
+                                LayerMask.GetMask("Actor","Blocking"));
+        
+        if (hit.collider == null) transform.Translate(moveDelta.x * Time.deltaTime, 0f, 0f);
+        
     }
 }
