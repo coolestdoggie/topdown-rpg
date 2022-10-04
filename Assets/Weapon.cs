e14781a112b4f3abf21b588ff1ca6eef4900f84e
coolestdoggie <coolestdoggie1@gmail.com>
2022-10-04 20:34:36 +0200

    add: weapon behaviour

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Weapon : Collidable
 {
-    // Start is called before the first frame update
-    void Start()
+    public int damagePoint = 1;
+    public float pushForce = 2f;
+
+    private float cooldown = 0.5f;
+    private float lastSwing;
+    
+    public int weaponLevel = 0;
+    private SpriteRenderer spriteRenderer;
+
+    protected override void Start()
     {
-        
+        base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time - lastSwing > cooldown)
+            {
+                lastSwing = Time.time;
+                Swing();
+            }
+        }
+    }
+
+    private void Swing()
+    {
+        throw new System.NotImplementedException();
     }
 }
