769bbf41da8deb7ffa57051b1093564e319cf5cf
coolestdoggie <coolestdoggie1@gmail.com>
2022-10-08 11:55:14 +0200

    fix: enemy behaviour

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Fighter
+public class Enemy : Mover
 {
-    
+    public int xpValue = 1;
+
+    public float triggerLength = 1f;
+    public float chaseLength = 5f;
+    private bool chasing;
+    private bool collidingWithPlayer;
+    private Transform playerTransform;
+    private Vector3 startingPosition;
+
+    private BoxCollider2D hitbox;
+    private Collider2D[] hits = new Collider2D[10];
 }
